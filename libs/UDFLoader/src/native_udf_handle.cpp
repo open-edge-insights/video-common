9fb92f44408acd4fd2b43f810e1e7453dee434a4
Midkiff, Kevin <kevin.midkiff@intel.com>
2019-10-30 00:34:24 -0700

    UDFLoader: Fix for CMake/compile warnings

--- libs/UDFLoader/src/native_udf_handle.cpp
+++ libs/UDFLoader/src/native_udf_handle.cpp
@@ -23,12 +23,12 @@
  * @author Caleb McMillan (caleb.m.mcmillan@intel.com)
  */
 
+#include <dlfcn.h>
+#include <unistd.h>
 #include <vector>
 #include <atomic>
 #include <iostream>
 #include <sstream>
-#include <dlfcn.h>
-#include <numpy/ndarrayobject.h>
 #include <eis/utils/logger.h>
 #include "eis/udf/native_udf_handle.h"
 
@@ -37,13 +37,13 @@
 using namespace eis::udf;
 using namespace eis::utils;
 
-NativeUdfHandle::NativeUdfHandle(std::string name, int max_workers) : 
+NativeUdfHandle::NativeUdfHandle(std::string name, int max_workers) :
 	UdfHandle(name, max_workers)
 {
 	m_lib_handle = NULL;
 	m_func_initialize_udf = NULL;
 	m_udf = NULL;
-} 
+}
 
 NativeUdfHandle::~NativeUdfHandle() {
 	LOG_DEBUG_0("Destroying Native UDF");
@@ -63,7 +63,7 @@ bool NativeUdfHandle::initialize(config_t* config) {
 		return false;
 	std::string name = get_name().c_str();
 	LOG_DEBUG("Loading native UDF: %s", name);
-	
+
 	LOG_DEBUG_0("Retrieving LD_LIBRARY_PATH");
     char* ld_library_path = getenv("LD_LIBRARY_PATH");
 
@@ -98,14 +98,14 @@ bool NativeUdfHandle::initialize(config_t* config) {
     if(found) {
         LOG_DEBUG("Found native UDF: %s", lib.c_str());
 		m_lib_handle = dlopen(lib.c_str(), RTLD_LAZY);
-		
+
 		if(!m_lib_handle) {
 			LOG_ERROR("Failed to load UDF library: %s", dlerror());
 			return false;
 		} else {
 			LOG_DEBUG_0("Successfully loaded UDF library");
 			*(void**)(&m_func_initialize_udf) = dlsym(m_lib_handle, "initialize_udf");
-	
+
 			if(!m_func_initialize_udf) {
 				LOG_ERROR("Failed to find initialize_udf symbol: %s", dlerror());
 				dlclose(m_lib_handle);
@@ -113,10 +113,10 @@ bool NativeUdfHandle::initialize(config_t* config) {
 			}
 			LOG_DEBUG_0("Successfully found initialize_udf symbol");
 		}
-		
+
 		void* udf = m_func_initialize_udf();
 		m_udf = (BaseUdf*) udf;
-	
+
 		if(!m_udf->initialize(config)) {
 			LOG_ERROR_0("Failed to initialize UDF");
 			return false;
@@ -137,7 +137,7 @@ UdfRetCode NativeUdfHandle::process(Frame* frame) {
 	//TODO: Do we want to default to 8bit?
 	cv::Mat* mat_frame = new cv::Mat(h, w, CV_8UC(c), frame->get_data());
 	msg_envelope_t* meta_data = frame->get_meta_data();
-	
+
 	UdfRetCode ret = m_udf->process(mat_frame, meta_data);
 	delete mat_frame;
 	mat_frame = NULL;

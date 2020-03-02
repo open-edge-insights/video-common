da8b05b51d08f2c6d2dcba44a593271c0dea55a4
Mcmillan, Caleb M <caleb.m.mcmillan@intel.com>
2020-03-02 01:13:22 -0800

    VideoIngestion: Optimized PCB filter 1. Added a scale ratio so that expensive CV functions are performance on a downsized frame to save resources and time. Accuracy is not affected when using a scale ratio of 4.

--- udfs/python/pcb/pcb_filter.py
+++ udfs/python/pcb/pcb_filter.py
@@ -23,43 +23,55 @@ import logging
 import cv2
 import numpy as np
 import time
+from datetime import datetime
+from random import uniform
 
 """Visual trigger for PCB anomaly detection.
 """
+
+
 class Udf:
     """PCB anomaly detection trigger object.
     """
-    def __init__(self, n_right_px, n_left_px, n_total_px, training_mode):
+
+    def __init__(self, n_right_px, n_left_px,
+                 n_total_px, training_mode, scale_ratio):
         """Udf constructor
 
-        :param n_right_px: minimum number of pixels to the right of PCB mask (default 1000)
+        :param n_right_px: minimum number of pixels
+                           to the right of PCB mask (default 1000)
         :type n_right_px: int
-        :param n_left_px: minimum number of pixels to the left of the PCB mask (default 1000)
+        :param n_left_px: minimum number of pixels
+                          to the left of the PCB mask (default 1000)
         :type n_left_px: int
-        :param n_total_px: minimum number of pixels in the PCB mask (default 300000)
+        :param n_total_px: minimum number of pixels
+                           in the PCB mask (default 300000)
         :type n_total_px: int
-        :param training_mode: flag to save image ROI's for training (default false)
+        :param training_mode: flag to save image ROI's
+                              for training (default false)
         :type training_mode: bool
         """
         self.log = logging.getLogger('PCB_FILTER')
         self.log.debug("In ctor")
+        self.ratio = scale_ratio
         # Initialize background subtractor
         self.fgbg = cv2.createBackgroundSubtractorMOG2()
         # Total white pixel # on MOG applied
         # frame after morphological operations
-        self.n_total_px = n_total_px
+        self.n_total_px = n_total_px/(self.ratio*self.ratio)
         # Total white pixel # on left edge of MOG
         # applied frame after morphological operations
-        self.n_left_px = n_left_px
+        self.n_left_px = n_left_px/(self.ratio*self.ratio)
         # Total white pixel # on right edge of MOG
         # applied frame after morphological operations
-        self.n_right_px = n_right_px
+        self.n_right_px = n_right_px/(self.ratio*self.ratio)
         # Flag to lock trigger from forwarding frames to classifier
         self.filter_lock = False
         self.training_mode = training_mode
         self.profiling = False
         self.count = 0
         self.lock_frame_count = 0
+        self.threads = 0
 
     def _check_frame(self, frame):
         """Determines if the given frame is the key frame of interest for
@@ -94,8 +106,8 @@ class Udf:
                     (n_right < self.n_right_px):
                 # Find the PCB contour
                 contours, hier = cv2.findContours(thresh.copy(),
-                                                      cv2.RETR_EXTERNAL,
-                                                      cv2.CHAIN_APPROX_NONE)
+                                                  cv2.RETR_EXTERNAL,
+                                                  cv2.CHAIN_APPROX_NONE)
                 if len(contours) != 0:
                     # Contour with largest area would be bounding the PCB
                     c = max(contours, key=cv2.contourArea)
@@ -109,7 +121,8 @@ class Udf:
                     # PCB doesn't touch the left or right edge
                     # of frame and the center x lies within
                     if (x != 0) & ((x + w) != columns) & \
-                       ((columns/2 - 100) <= cX <= (columns/2 + 100)):
+                       ((columns/2 - (100/self.ratio)) <= cX and
+                       cX <= (columns/2 + (100/self.ratio))):
                         return True
                     else:
                         return False
@@ -128,6 +141,9 @@ class Udf:
         """
         if self.profiling is True:
             metadata['ts_vi_filter_entry'] = time.time()*1000
+        frame_height, frame_width = frame.shape[:-1]
+        resized_frame = cv2.resize(frame, (int(frame_width/self.ratio),
+                                           int(frame_height/self.ratio)))
 
         if self.training_mode is True:
             self.count = self.count + 1
@@ -135,7 +151,7 @@ class Udf:
             return True, None, None
         else:
             if self.filter_lock is False:
-                if self._check_frame(frame):
+                if self._check_frame(resized_frame):
                     self.filter_lock = True
                     # Re-initialize frame count during trigger lock to 0
                     self.lock_frame_count = 0
@@ -145,7 +161,7 @@ class Udf:
             else:
                 # Continue applying background subtractor to
                 # keep track of PCB positions
-                self._check_frame(frame)
+                self._check_frame(resized_frame)
                 # Increment frame count during trigger lock phase
                 self.lock_frame_count = self.lock_frame_count + 1
                 if self.lock_frame_count == 7:
